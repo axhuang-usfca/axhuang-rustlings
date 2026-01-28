a7958bf8ffc3d7d80c54832a966722049af218d7
Andrew Huang <axhuang@dons.usfca.edu>
2026-01-28 00:59:13 -0800

    pre lab rustling done

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,19 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let x = 12;
+    let y = 10;
+    println!("between {x} and {y}, {} is bigger", bigger(x, y));
 }
 
 // Don't mind this for now :)
--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -16,6 +16,14 @@ fn main() {
     // You can optionally experiment here.
 }
 
+fn calculate_price_of_apples(input: i32) -> i32 {
+    if input > 40 {
+        input
+    } else {
+        input * 2
+    }
+}
+
 // Don't change the tests!
 #[cfg(test)]
 mod tests {
