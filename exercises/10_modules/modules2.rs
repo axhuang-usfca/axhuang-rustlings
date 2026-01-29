77c2be2f07b9559bc6c31ff6bee3822f0b04c151
Andrew Huang <axhuang@dons.usfca.edu>
2026-01-28 22:37:10 -0800

    1/28 HW done

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -15,9 +15,13 @@ mod delicious_snacks {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
+
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 }
 
 fn main() {
+
     println!(
         "favorite snacks: {} and {}",
         delicious_snacks::fruit,
